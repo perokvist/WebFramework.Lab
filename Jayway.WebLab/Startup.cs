6efde39fca1a4ddc39ada157e189f9bb256dccba
perokvist <per_okvist@live.se>
2014-02-26 14:05:53 +0100

    Lab Init

---
+++ Jayway.WebLab/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace Jayway.WebLab
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var controllers = Assembly.GetExecutingAssembly().GetTypes().Where(x => x.Name.EndsWith("Controller"));
+
+            app
+                .Use((ctx, next) => ctx.Request.Uri.ToString().Contains("localhost") ? next() : Task.Run(() => ctx.Response.Write("Not Authorized")))
+                .Use((ctx, next) =>
+                {
+                    var routePart = ctx.Request.Uri.Segments.Last().Trim('/').ToLower();
+                    var controllerType = controllers.First(x => x.Name.ToLower().StartsWith(routePart));
+                    return controllerType != null ? ((dynamic)Activator.CreateInstance(controllerType)).Run((dynamic)ctx) : next();
+                })
+                .Run(context => context.Response.WriteAsync("No route found"));
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+
+        public class TestController
+        {
+            public Task Run(IOwinContext context)
+            {
+                return context.Response.WriteAsync("Boom!");
+            }
+        }
+    }
+}
